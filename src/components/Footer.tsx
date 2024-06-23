9df0823778133385fa50087a42a94c19f8486a8a
JuniorFixHow <robertjuniorannan@gmail.com>
2024-06-23 03:39:01 +0100

    font sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className="w-5/6 flex flex-col justify-center items-center md:flex-row md:items-start md:justify-around">
             <div className="flex flex-row items-start gap-8">
 
-            <div className="flex flex-col py-4 items-start">
+            <div className="flex flex-col py-4 items-start min-w-32">
                 <h2 className="text-2xl text-[#CB4900] font-bold">Links</h2>
                 <a href="#hero" className="text-black underline hover:text-blue-500 text-[1rem] font-semibold cursor-pointer" >Home</a>
                 <a href="#services" className="text-black underline hover:text-blue-500 text-[1rem] font-semibold cursor-pointer" >Services</a>
@@ -26,7 +26,7 @@ const Footer = () => {
             </div>
             </div>
             <div className="flex flex-col gap-4">
-                <h2 className="font-semibold text-black text-[1.5rem]" >Subscribe to our <span className="text-[#CB4900]" >Newsletter</span></h2>
+                <h2 className="font-semibold text-[1rem] text-black md:text-[1.5rem]" >Subscribe to our <span className="text-[#CB4900]" >Newsletter</span></h2>
                 <div className="flex flex-row items-center justify-center">
                     <input type="text" className="bg-[#d9d9d9] outline-none px-4 py-2 w-72" placeholder="Email" />
                     <div className="bg-[#CB4900] hover:bg-orange-500 cursor-pointer h-10 w-8 flex items-center justify-center" ><FaAngleRight /></div>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -16,12 +16,12 @@ const Services = () => {
     }
   return (
     <section id="services" className="w-5/6 flex flex-col py-8 self-center gap-8 items-center justify-center" >
-        <h2 className="text-black text-2xl sm:text-3xl font-bold" ><span className="text-[#CB4900]" >Services</span> We Can Do For You</h2>
+        <h2 className="text-black text-center text-2xl sm:text-3xl font-bold" ><span className="text-[#CB4900]" >Services</span> We Can Do For You</h2>
         <div className="grid place-items-center grid-cols-1 md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full">
             {
                 ServiceData &&
                 ServiceData.map((item:ServicesProps)=>(
-                    <div onClick={()=>handleOpenService(item)} key={item?.id} className="rounded-2xl hover:cursor-pointer flex flex-col item-center justify-center w-80 md:w-5/6 shadow-xl pb-4">
+                    <div onClick={()=>handleOpenService(item)} key={item?.id} className="rounded-2xl hover:cursor-pointer flex flex-col item-center justify-center w-80 md:w-72 shadow-xl pb-4">
                         <img src={item?.image} className=" rounded-2xl w-full h-48 object-cover" alt="" />
                         <span className="text-black font-medium text-[1rem] mt-4 text-center">{item?.title}</span>
                     </div>
