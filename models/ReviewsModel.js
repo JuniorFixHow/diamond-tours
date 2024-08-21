3a946e031aa803cd4f7857fc04e19b752ffa7050
JuniorFixHow <robertjuniorannan@gmail.com>
2024-08-21 08:20:24 +0100

    fixed mogoose models

--- models/ReviewsModel.js
+++ models/ReviewsModel.js
@@ -1,10 +1,13 @@
-import { model, models, Schema } from "mongoose"
+import mongoose from "mongoose"
 
-const ReviewSchema = new Schema({
+const ReviewSchema = new mongoose.Schema({
     user:{
         name:String,
         id:String,
-        photo:String
+        photo:{
+            type:String,
+            default:'https://cdn-icons-png.flaticon.com/512/9187/9187604.png'
+        }
     },
     isReply:{
         type:Boolean,
@@ -14,6 +17,6 @@ const ReviewSchema = new Schema({
     content:String
 },{timestamps:true})
 
-const Review = models.Review || model('Review', ReviewSchema);
+const Review = mongoose.models.Review || mongoose.model('Review', ReviewSchema);
 export default Review;
 // 'https://cdn-icons-png.flaticon.com/512/9187/9187604.png'
