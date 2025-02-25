483079c1f8e31ce7544332e17b4461c556e77137
JuniorFixHow <robertjuniorannan@gmail.com>
2025-02-25 13:05:06 +0000

    finished card

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -27,15 +27,16 @@ const MobileMenu = ({showMenu, setShowMenu}:menuProps) =>{
 return (
   <section onClick={()=>setShowMenu('hidden')} className={menulass} id="mobile-menu" >
       <button onClick={()=>setShowMenu('hidden')} className="text-6xl self-end px-6">&times;</button>
-      <nav className="flex flex-col min-h-screen w-full items-center py-4" aria-label="mobile" >
-          <a onClick={()=>setShowMenu('hidden')} href="/#hero" className="w-full text-center text-2xl py-2 hover:opacity-90 text-white" >Home</a>
-          <a onClick={()=>setShowMenu('hidden')} href="/#services" className="w-full text-center text-2xl py-2 hover:opacity-90 text-white" >Services</a>
-          <a onClick={()=>setShowMenu('hidden')} href="/#packages" className="w-full text-center text-2xl py-2 hover:opacity-90 text-white" >Packages</a>
-          <a onClick={handleBooking} href="#bookings" className="w-full text-center text-2xl py-2 hover:opacity-90 text-white" >Bookings</a>
-          <a onClick={()=>setShowMenu('hidden')} href="/#about" className="w-full text-center text-2xl py-2 hover:opacity-90 text-white" >About Us</a>
-          <a onClick={()=>setShowMenu('hidden')} href="/blogs" className="w-full text-center text-2xl py-2 hover:opacity-90 text-white" >Blogs</a>
-          <a onClick={()=>setShowMenu('hidden')} href="/#contact" className="w-full text-center text-2xl py-2 hover:opacity-90 text-white" >Contact Us</a>
-          <a onClick={()=>setShowMenu('hidden')} href="/#footer" className="w-full text-center text-2xl py-2 hover:opacity-90 text-white" >Legal</a>
+      <nav className="flex flex-col gap-1 min-h-screen w-full items-center py-4" aria-label="mobile" >
+          <a onClick={()=>setShowMenu('hidden')} href="/#hero" className="w-full text-center text-2xl  hover:opacity-90 text-white" >Home</a>
+          <a onClick={()=>setShowMenu('hidden')} href="/#services" className="w-full text-center text-2xl  hover:opacity-90 text-white" >Services</a>
+          <a onClick={()=>setShowMenu('hidden')} href="/#packages" className="w-full text-center text-2xl  hover:opacity-90 text-white" >Packages</a>
+          {/* <a onClick={()=>setShowMenu('hidden')} href="/#new-packages" className="w-full text-center text-2xl  hover:opacity-90 text-white" >Available Packages</a> */}
+          <a onClick={handleBooking} href="#bookings" className="w-full text-center text-2xl  hover:opacity-90 text-white" >Bookings</a>
+          <a onClick={()=>setShowMenu('hidden')} href="/#about" className="w-full text-center text-2xl  hover:opacity-90 text-white" >About Us</a>
+          <a onClick={()=>setShowMenu('hidden')} href="/blogs" className="w-full text-center text-2xl  hover:opacity-90 text-white" >Blogs</a>
+          <a onClick={()=>setShowMenu('hidden')} href="/#contact" className="w-full text-center text-2xl  hover:opacity-90 text-white" >Contact Us</a>
+          <a onClick={()=>setShowMenu('hidden')} href="/#footer" className="w-full text-center text-2xl  hover:opacity-90 text-white" >Legal</a>
           <div className="gap-6 flex-col flex justify-center md:hidden w-full items-center">
               <div className="rounded-2xl w-1/2 flex items-center justify-center cursor-pointer hover:bg-slate-300 border border-black bg-white text-black px-4 py-2">
                 <MdOutlinePhoneAndroid size={25} />
